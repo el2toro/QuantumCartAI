c2047fcfaeb686d6acfd5c074f480d125a1179e0
el2toro <vasea.badragan@gmail.com>
2025-11-07 13:06:16 +0100

    Configure Carter and MediatR

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,21 @@
+using Carter;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = Assembly.GetAssembly(typeof(Program));
 // Add services to the container.
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly!);
+});
+
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapCarter();
 
 app.MapGet("/", () =>
 {

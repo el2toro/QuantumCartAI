44daa03e880e010fcbcf7334b01e2e1130bed26e
el2toro <vasea.badragan@gmail.com>
2025-11-07 13:01:18 +0100

    Define GetCatalog endpoint

---
+++ Services/Catalog/Catalog.API/Endpoints/CatalogEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Carter;
+using MediatR;
+
+namespace Catalog.API.Endpoints;
+
+public class CatalogEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("tenants/{tenantId}/catalog", (ISender sender) =>
+        {
+
+        })
+        .WithDisplayName("GetCatalog")
+        .WithDescription("GetCatalog")
+        .Produces(StatusCodes.Status200OK)
+        .WithSummary("Get Catalog");
+    }
+}

ec60b53e52a5bfcb8e0fab7945ec191354febb37
el2toro <vasea.badragan@gmail.com>
2025-11-13 14:47:47 +0100

    Add models

---
+++ Services/Catalog/Catalog.API/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+
+    // Navigation property
+    public ICollection<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
+}
+
+public class ProductCategory
+{
+    public Guid ProductId { get; set; }
+    public Product Product { get; set; } = default!;
+
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; } = default!;
+}
---
+++ Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string Sku { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+    public int Rating { get; set; }
+    public bool IsAvailable { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    // Navigation property
+    public ICollection<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
+}

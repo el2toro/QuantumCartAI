f9fa7e1155c490a59f8d2034f0cfe78303313150
el2toro <vasea.badragan@gmail.com>
2025-11-13 14:48:55 +0100

    Add GetProducts endpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+using MediatR;
+
+namespace Catalog.API.Products.GetProducts;
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("catalog", async (ISender sender) =>
+        {
+            var data = await sender.Send(new GetProductsQuery());
+            return Results.Ok(data.Products);
+        })
+       .WithDisplayName("GetCatalog")
+       .WithDescription("GetCatalog")
+       .Produces(StatusCodes.Status200OK)
+       .WithSummary("Get Catalog");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IRequest<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+public class GetProductsHandler(ProductDbContext productDbContext) : IRequestHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await productDbContext.Products.ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
+}

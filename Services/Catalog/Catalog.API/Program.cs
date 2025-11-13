c10d769a8fa562be19c05a8f7a5b83a34de55975
el2toro <vasea.badragan@gmail.com>
2025-11-13 14:50:56 +0100

    Configure dbContext and health check

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,9 @@
 using Carter;
+using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,15 +17,29 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddCarter();
 
+builder.Services.AddDbContext<ProductDbContext>(config =>
+{
+    config.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
+
+// Add Health Checks
+builder.Services.AddHealthChecks()
+    .AddNpgSql(
+      builder.Configuration.GetConnectionString("Database")!,
+      name: "PostgreSQL",
+      failureStatus: HealthStatus.Degraded);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 
-app.MapGet("/", () =>
-{
-    return "This is working!";
-});
+// Map the health check endpoint
+app.MapHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
 

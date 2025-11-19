5ca8543d3ea4ba7ef3f5654cd6e5cf518f491522
el2toro <vasea.badragan@gmail.com>
2025-11-19 18:11:02 +0100

    Add CartItemAddedEventHandler

---
+++ Services/Cart/Cart.Application/EventHandlers/CartItemAddedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using Cart.Domain.Events;
+using MassTransit;
+
+namespace Cart.Application.EventHandlers;
+
+public class CartItemAddedEventHandler : IConsumer<CartItemAddedEvent>
+{
+    public Task Consume(ConsumeContext<CartItemAddedEvent> context)
+    {
+        //throw new NotImplementedException();
+        return Task.CompletedTask;
+    }
+}

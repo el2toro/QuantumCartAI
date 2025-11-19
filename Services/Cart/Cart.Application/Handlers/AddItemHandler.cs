edcda027d228b6c5cca14cd0c55a495de4c9a314
el2toro <vasea.badragan@gmail.com>
2025-11-19 17:22:47 +0100

    Add handlers

---
+++ Services/Cart/Cart.Application/Handlers/AddItemHandler.cs
@@ -0,0 +1,41 @@
+﻿using Cart.Application.Commands;
+using MediatR;
+using Cart.Domain.ValueObjects;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Cart.Application.Handlers;
+
+public class AddItemHandler(IDistributedCache distributedCache)
+    : IRequestHandler<AddItemCommand, AddItemResult>
+{
+    public async Task<AddItemResult> Handle(AddItemCommand command, CancellationToken cancellationToken)
+    {
+        // Create a cart instance if CustomerId is not provided
+
+
+        var cart = new Domain.Entities.Cart(CartId.New(), null!);
+        // 1. Validate ProductId
+        // 2. Call Pricing Service to get current price
+        // 5. Publish event item added to cart
+
+
+
+        // 3. Add item to domain aggregate
+        cart.AddItem(
+            ProductId.From(command.ProductId.ToString()),
+            Quantity.From(command.Quantity),
+            new Money(50));
+
+        // 4. Save the cart (Redis/Mongo/etc.)
+        distributedCache.SetString(
+            cart.Id.ToString(),
+            JsonSerializer.Serialize(cart),
+            new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1)
+            });
+
+        return new AddItemResult(true);
+    }
+}
---
+++ Services/Cart/Cart.Application/Handlers/CreateCartHandler.cs
@@ -0,0 +1,26 @@
+﻿using Cart.Application.Commands;
+using Microsoft.Extensions.Caching.Distributed;
+using MediatR;
+using System.Text.Json;
+
+namespace Cart.Application.Handlers;
+
+public class CreateCartHandler(IDistributedCache distributedCache)
+    : IRequestHandler<CreateCartCommand, CreateCartResult>
+{
+    public async Task<CreateCartResult> Handle(CreateCartCommand request, CancellationToken cancellationToken)
+    {
+        var cart = new Domain.Entities.Cart(
+            new Domain.ValueObjects.CartId(request.CartId),
+             new Domain.ValueObjects.CustomerId(request.CartId));
+
+
+        var existingCart = await distributedCache.GetStringAsync(request.CartId.ToString());
+        if (existingCart is not null)
+        {
+            return JsonSerializer.Deserialize<CreateCartResult>(existingCart)!;
+        }
+
+        return new CreateCartResult();
+    }
+}

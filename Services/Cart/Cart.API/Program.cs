3a0f27695de9ff8e88a5cfa76a1003b458df7c86
el2toro <vasea.badragan@gmail.com>
2025-11-20 11:17:50 +0100

    Implement catalog grpc service

--- Services/Cart/Cart.API/Program.cs
+++ Services/Cart/Cart.API/Program.cs
@@ -1,11 +1,15 @@
 using BuildingBlocks.Messaging.MassTransit;
+using Cart.API.Services;
 using Cart.Application.EventHandlers;
 using Cart.Application.Handlers;
+using Cart.Application.Interfaces;
+using Catalog.gRPC;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
+builder.Services.AddScoped<ICatalogGrpcService, CatalogGrpcServiceClient>();
 builder.Services.AddCarter();
 builder.Services.AddMediatR(cfg =>
 {
@@ -28,9 +32,26 @@ var eventHandlerAssemblies = new Assembly[]
 
 builder.Services.AddMessageBroker(builder.Configuration, eventHandlerAssemblies);
 
+//gRPC Services
+builder.Services.AddGrpcClient<CatalogQueryService.CatalogQueryServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:CatalogUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 app.MapCarter();
-
+app.MapGet("grpc", () => "OK");
 app.Run();
 
---
+++ Services/Cart/Cart.API/Services/CatalogGrpcServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using Cart.Application.Dtos;
+using Cart.Application.Interfaces;
+using Catalog.gRPC;
+
+namespace Cart.API.Services;
+
+public class CatalogGrpcServiceClient(CatalogQueryService.CatalogQueryServiceClient catalogQueryServiceClient) : ICatalogGrpcService
+{
+    public async Task<ProductQueryDto> GetProduct(Guid productId)
+    {
+        var request = new CatalogQueryRequest { Id = productId.ToString() };
+        var response = await catalogQueryServiceClient.GetProductAsync(request);
+
+        return new ProductQueryDto(response.ProductExists);
+    }
+}

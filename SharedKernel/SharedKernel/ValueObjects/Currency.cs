b2c0c1271bd7e9a13c0f32d20404915526c6bba1
el2toro <vasea.badragan@gmail.com>
2025-11-21 15:31:27 +0100

    Add shared kernel

---
+++ SharedKernel/SharedKernel/ValueObjects/Currency.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedKernel.ValueObjects;
+
+public enum Currency
+{
+    None = 1,
+    USD = 2,
+    EUR = 3,
+    GBP = 4
+}
---
+++ SharedKernel/SharedKernel/ValueObjects/Money.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedKernel.ValueObjects;
+
+public record Money(decimal Amount, Currency Currency = Currency.USD)
+{
+    public static Money Zero(Currency currency) => new(0m, currency);
+    public Money Multiply(Quantity qty) => this with { Amount = Amount * qty.Value };
+
+    public static Money operator +(Money a, Money b)
+    {
+        if (a.Currency != b.Currency)
+            throw new InvalidOperationException("Cannot add Money with different currencies.");
+        return new Money(a.Amount + b.Amount, a.Currency);
+    }
+}

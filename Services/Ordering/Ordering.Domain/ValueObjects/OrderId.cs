97a25514f5df7f36f18118ad1b82a32639fb6b25
el2toro <vasea.badragan@gmail.com>
2025-11-21 15:34:46 +0100

    Add value onjects

---
+++ Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record OrderId(Guid Value)
+{
+    public static OrderId New() => new(Guid.NewGuid());
+    public static OrderId From(string value) => new(Guid.Parse(value));
+}
---
+++ Services/Ordering/Ordering.Domain/ValueObjects/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public enum OrderStatus
+{
+    Submitted = 1,
+    AwaitingPayment = 2,
+    Paid = 3,
+    Shipped = 4,
+    Completed = 5,
+    Cancelled = 6
+}

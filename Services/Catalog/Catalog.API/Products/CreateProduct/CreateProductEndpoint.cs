f860ed7d72b2d19178621d3ec09e98e16eae6c07
el2toro <vasea.badragan@gmail.com>
2025-11-25 21:24:24 +0100

    Add CreateProduct endpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public record CreateProductRequest(
+        string Name,
+        string Description,
+        string Sku,
+        string ImageFile,
+        decimal Price,
+        int Rating,
+        List<Guid> Categories);
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("catalog", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+
+            return Results.Created($"catalog/products/{request.Name}", result);
+        });
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.DTOs;
+using Catalog.API.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+       string Name,
+        string Description,
+        string Sku,
+        string ImageFile,
+        decimal Price,
+        int Rating,
+        List<Guid> Categories) : ICommand<CreateProductResult>;
+public record CreateProductResult(ProductDto Product);
+public class CreateProductHandler(IProductRepository productRepository)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var productToBeCreated = command.Adapt<Product>();
+        productToBeCreated.CreatedAt = DateTime.UtcNow;
+        productToBeCreated.UpdatedAt = DateTime.UtcNow;
+
+        if (command.Categories.Any())
+        {
+            foreach (var categoryId in command.Categories)
+            {
+                productToBeCreated.ProductCategories.Add(new ProductCategory { CategoryId = categoryId });
+            }
+        }
+
+        var createdProduct = await productRepository.CreateProduct(productToBeCreated, cancellationToken);
+        var result = createdProduct.Adapt<ProductDto>();
+
+        return new CreateProductResult(result);
+    }
+}

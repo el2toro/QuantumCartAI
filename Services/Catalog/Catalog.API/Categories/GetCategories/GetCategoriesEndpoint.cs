ed863e6b5cf6c5fbd0f95fc612839651a5f9beab
el2toro <vasea.badragan@gmail.com>
2025-12-01 10:35:38 +0100

    Implement GetCategory endpoint and handler

---
+++ Services/Catalog/Catalog.API/Categories/GetCategories/GetCategoriesEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Categories.GetCategories;
+
+public class GetCategoriesEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("categories", async (ISender sender) =>
+        {
+            var response = await sender.Send(new GetCategoriesQuery());
+
+            return Results.Ok(response.Categories);
+        })
+       .WithDisplayName("GetCategories")
+       .WithDescription("GetCategories")
+       .Produces(StatusCodes.Status200OK)
+       .WithSummary("Get Categories");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Categories/GetCategories/GetCategoriesHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.DTOs;
+using Catalog.API.Repositories;
+
+namespace Catalog.API.Categories.GetCategories;
+
+public record GetCategoriesQuery() : IQuery<GetCategoriesResult>;
+public record GetCategoriesResult(IEnumerable<CategoryDto> Categories);
+public class GetCategoriesHandler(ICategoryRepository categoryRepository)
+    : IQueryHandler<GetCategoriesQuery, GetCategoriesResult>
+{
+    public async Task<GetCategoriesResult> Handle(GetCategoriesQuery query, CancellationToken cancellationToken)
+    {
+        var categories = await categoryRepository.GetCategoriesAsync(cancellationToken);
+        var result = categories.Adapt<IEnumerable<CategoryDto>>();
+
+        return new GetCategoriesResult(result);
+    }
+}

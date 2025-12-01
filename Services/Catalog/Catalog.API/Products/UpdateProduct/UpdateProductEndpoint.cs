155d0d9135021095cfe3bade130872a064e966c6
el2toro <vasea.badragan@gmail.com>
2025-12-01 10:35:02 +0100

    Implement UpdateProduct endpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("catalog/products", async (ProductDto request, ISender sender) =>
+        {
+            var result = await sender.Send(new UpdateProductCommand(request));
+
+            return Results.Ok(result.Product);
+        })
+     .WithDisplayName("UpdateProduct")
+     .WithDescription("UpdateProduct")
+     .Produces(StatusCodes.Status200OK)
+     .WithSummary("Update Product");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(ProductDto Product) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(ProductDto Product);
+public class UpdateProductHandler(IProductRepository productRepository)
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        var productToBeUpdated = await productRepository
+            .GetProductById(command.Product.Id, cancellationToken)
+            ?? throw new ProductNotFoundException(command.Product.Id.ToString());
+
+        productToBeUpdated.Sku = command.Product.Sku;
+        productToBeUpdated.Name = command.Product.Name;
+        productToBeUpdated.Description = command.Product.Description;
+        productToBeUpdated.Price = command.Product.Price;
+        productToBeUpdated.ImageFile = command.Product.ImageFile;
+        productToBeUpdated.IsAvailable = command.Product.IsAvailable;
+        productToBeUpdated.Rating = command.Product.Rating;
+        productToBeUpdated.UpdatedAt = DateTime.UtcNow;
+
+
+
+        //productToBeUpdated.ProductCategories = command.Product.CategoriesId.Any()
+        //    ? command.Product.CategoriesId
+        //     .Select(id => new ProductCategory { CategoryId = id, ProductId = command.Product.Id })
+        //     .ToList()
+        //   : [];
+
+        var updatedProduct = await productRepository.UpdateProduct(productToBeUpdated, cancellationToken);
+        var result = updatedProduct.Adapt<ProductDto>();
+
+        return new UpdateProductResult(result);
+    }
+}

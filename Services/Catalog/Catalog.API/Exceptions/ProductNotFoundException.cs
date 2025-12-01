7ec693e5fa6d3aca30f1bacbf6ab4145e2a2f68b
el2toro <vasea.badragan@gmail.com>
2025-12-01 10:37:38 +0100

    Add product not found exception

---
+++ Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() { }
+        public ProductNotFoundException(string message) : base(message) { }
+    }
+}

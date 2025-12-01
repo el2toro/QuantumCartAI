6e5a3d8cc4839504e2c60cccb5cb9f2f7f2748b1
el2toro <vasea.badragan@gmail.com>
2025-12-01 10:35:19 +0100

    Implement GetProductByCategory endpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductByCategoryIdHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public record GetProductsByCategoryIdQuery(Guid CategoryId) : IQuery<GetProductByCategoryIdResult>;
+public record GetProductByCategoryIdResult(IEnumerable<ProductDto> Products);
+
+public class GetProductByCategoryIdHandler(IProductRepository productRepository)
+    : IQueryHandler<GetProductsByCategoryIdQuery, GetProductByCategoryIdResult>
+{
+    public async Task<GetProductByCategoryIdResult> Handle(GetProductsByCategoryIdQuery query, CancellationToken cancellationToken)
+    {
+        var products = await productRepository.GetProductsByCategoryAsync(query.CategoryId, cancellationToken);
+        var result = products.Adapt<IEnumerable<ProductDto>>();
+
+        return new GetProductByCategoryIdResult(result);
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryIdEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public class GetProductsByCategoryIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("catalog/products/category/{categoryId}", async (Guid categoryId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsByCategoryIdQuery(categoryId));
+
+            return Results.Ok(result.Products);
+        })
+       .WithDisplayName("GetProductsByCategoryId")
+       .WithDescription("GetProductsByCategoryId")
+       .Produces(StatusCodes.Status200OK)
+       .WithSummary("Get Products By Category Id");
+    }
+}

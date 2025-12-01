d655fb267a26772f2abb4848810b75fc02d395ef
el2toro <vasea.badragan@gmail.com>
2025-12-01 10:34:45 +0100

    Implement GetProduct ByIdendpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("catalog/products/{productId}", async (Guid productId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(productId));
+
+            return Results.Ok(result.Product);
+        })
+       .WithDisplayName("GetProductById")
+       .WithDescription("GetProductById")
+       .Produces(StatusCodes.Status200OK)
+       .WithSummary("Get Product By Id");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid ProductId) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(ProductDto Product);
+public class GetProductByIdHandler(IProductRepository productRepository)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await productRepository
+            .GetProductById(query.ProductId, cancellationToken)
+            ?? throw new ProductNotFoundException(query.ProductId.ToString());
+
+        var result = product.Adapt<ProductDto>();
+
+        return new GetProductByIdResult(result);
+    }
+}

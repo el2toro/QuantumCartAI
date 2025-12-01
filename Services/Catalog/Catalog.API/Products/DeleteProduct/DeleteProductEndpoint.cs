c77e71b94eb95c0d71f5fabf36378f848b469450
el2toro <vasea.badragan@gmail.com>
2025-12-01 10:34:32 +0100

    Implement DeleteProduct endpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("catalog/products/{productId}", async (Guid productId, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(productId));
+
+            return Results.NoContent();
+        })
+       .WithDisplayName("DeleteProduct")
+       .WithDescription("DeleteProduct")
+       .Produces(StatusCodes.Status204NoContent)
+       .WithSummary("Delete Product");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid ProductId) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+public class DeleteProductHandler(IProductRepository productRepository)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = await productRepository
+            .GetProductById(command.ProductId, cancellationToken)
+            ?? throw new ProductNotFoundException(command.ProductId.ToString());
+
+        await productRepository.DeleteProduct(product, cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}

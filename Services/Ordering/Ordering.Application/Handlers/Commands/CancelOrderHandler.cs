4bfdf69f28e8ece36f9eb49a4b10116fa8b67af8
el2toro <vasea.badragan@gmail.com>
2025-12-02 13:52:08 +0100

    Add handlers

---
+++ Services/Ordering/Ordering.Application/Handlers/Commands/CancelOrderHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+using MassTransit;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Handlers.Commands;
+
+public record CancelOrderCommand() : ICommand<CancelOrderResult>;
+public record CancelOrderResult();
+
+public class CancelOrderHandler(IPublishEndpoint publishEndpoin) : ICommandHandler<CancelOrderCommand, CancelOrderResult>
+{
+    public async Task<CancelOrderResult> Handle(CancelOrderCommand command, CancellationToken cancellationToken)
+    {
+        await publishEndpoin.Publish<OrderCanceledEvent>("result", cancellationToken);
+        return new CancelOrderResult();
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Handlers/Commands/CreateOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+using MassTransit;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Handlers.Commands;
+
+public record CreateOrderCommand() : ICommand<CreateOrderResult>;
+public record CreateOrderResult();
+public class CreateOrderHandler(IPublishEndpoint publishedEndpoint) : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        await publishedEndpoint.Publish<OrderCreatedEvent>("result", cancellationToken);
+        return new CreateOrderResult();
+    }
+}

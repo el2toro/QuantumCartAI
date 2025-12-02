a61acde1b0096acfb2ee8dd8d36947361573c50d
el2toro <vasea.badragan@gmail.com>
2025-12-02 13:52:16 +0100

    Add events

--- Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
+++ Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Ordering.Domain.Events;
 
-namespace Ordering.Domain.Events
+public class OrderCreatedEvent
 {
-    internal class OrderCreatedEvent
-    {
-    }
 }

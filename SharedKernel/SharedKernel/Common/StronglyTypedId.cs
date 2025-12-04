d04a614b228bb3e6ff454ef3fe1d65eb39140c10
el2toro <vasea.badragan@gmail.com>
2025-12-04 15:04:14 +0100

    Create base entity

---
+++ SharedKernel/SharedKernel/Common/StronglyTypedId.cs
@@ -0,0 +1,7 @@
+﻿namespace SharedKernel.Common;
+
+public abstract record StronglyTypedId<TValue>(TValue Value)
+    where TValue : notnull
+{
+    public override string ToString() => Value.ToString();
+}

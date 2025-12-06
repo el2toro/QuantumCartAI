07c1922244b961ca3c1b9cd28557027ec48ea09f
el2toro <vasea.badragan@gmail.com>
2025-12-06 10:06:41 +0100

    Configure database

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,15 @@
 using BuildingBlocks.Messaging.MassTransit;
 using Carter;
+using Microsoft.EntityFrameworkCore;
 using Ordering.Application.Handlers.Commands;
+using Ordering.Application.Interfaces;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IOrderingRepository, OrderingRepository>();
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
@@ -13,11 +18,16 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(CancelOrderHandler).Assembly);
 });
 
+builder.Services.AddDbContext<OrderingDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
 
 builder.Services.AddMessageBroker(builder.Configuration, [typeof(CreateOrderHandler).Assembly]);
 
 var app = builder.Build();
-app.MapCarter();
+
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();

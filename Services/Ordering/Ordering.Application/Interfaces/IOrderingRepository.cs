176564f69b246a3ef7f6a58dae2098257419f107
el2toro <vasea.badragan@gmail.com>
2025-12-06 10:04:02 +0100

    Define ordering repository contract

---
+++ Services/Ordering/Ordering.Application/Interfaces/IOrderingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Interfaces;
+
+public interface IOrderingRepository
+{
+    Task<IEnumerable<Order>> GetOrdersAsync(Guid customerId, CancellationToken cancellationToken);
+    Task<Order> GetOrderByIdAsync(Guid customerId, Guid orderId, CancellationToken cancellationToken);
+    Task<Order> CreateOrderAsync(Order order, CancellationToken cancellationToken);
+}

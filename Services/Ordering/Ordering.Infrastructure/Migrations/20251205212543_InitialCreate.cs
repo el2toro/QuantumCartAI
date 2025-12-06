5a2e35449dd6e42179b4109fa1bd5ac58261330b
el2toro <vasea.badragan@gmail.com>
2025-12-06 10:05:22 +0100

    Add migration

---
+++ Services/Ordering/Ordering.Infrastructure/Migrations/20251205212543_InitialCreate.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ordering");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ShippingStreet = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ShippingCity = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ShippingState = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ShippingCountry = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ShippingZipCode = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    BillingStreet = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    BillingCity = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BillingState = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    BillingCountry = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    BillingZipCode = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    PaymentStatus = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    PaidDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    ShippedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    DeliveredDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    CancelledDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    CancellationReason = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    AppliedDiscount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    DiscountCode = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ShippingCost = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    CustomerNotes = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvoiceNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    PaidDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    OrderId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Invoices_Orders_OrderId1",
+                        column: x => x.OrderId1,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ProductImageUrl = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ProductSku = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Discount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderStatusHistories",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ChangedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Notes = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    ChangedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    Metadata = table.Column<string>(type: "jsonb", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStatusHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderStatusHistories_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    PaymentMethod = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    GatewayTransactionId = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shipments",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TrackingNumber = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Carrier = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ShippingCost = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ShippedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DeliveredDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    EstimatedDeliveryDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shipments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shipments_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_InvoiceNumber",
+                schema: "ordering",
+                table: "Invoices",
+                column: "InvoiceNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_IssueDate",
+                schema: "ordering",
+                table: "Invoices",
+                column: "IssueDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_OrderId",
+                schema: "ordering",
+                table: "Invoices",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_OrderId1",
+                schema: "ordering",
+                table: "Invoices",
+                column: "OrderId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_Status",
+                schema: "ordering",
+                table: "Invoices",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                schema: "ordering",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                schema: "ordering",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId_OrderDate",
+                schema: "ordering",
+                table: "Orders",
+                columns: new[] { "CustomerId", "OrderDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderDate",
+                schema: "ordering",
+                table: "Orders",
+                column: "OrderDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderNumber",
+                schema: "ordering",
+                table: "Orders",
+                column: "OrderNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaidDate",
+                schema: "ordering",
+                table: "Orders",
+                column: "PaidDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaymentStatus",
+                schema: "ordering",
+                table: "Orders",
+                column: "PaymentStatus");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_Status",
+                schema: "ordering",
+                table: "Orders",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_Status_OrderDate",
+                schema: "ordering",
+                table: "Orders",
+                columns: new[] { "Status", "OrderDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderStatusHistories_OrderId",
+                schema: "ordering",
+                table: "OrderStatusHistories",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderStatusHistories_OrderId_ChangedAt",
+                schema: "ordering",
+                table: "OrderStatusHistories",
+                columns: new[] { "OrderId", "ChangedAt" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderStatusHistories_OrderId_Status_ChangedAt",
+                schema: "ordering",
+                table: "OrderStatusHistories",
+                columns: new[] { "OrderId", "Status", "ChangedAt" },
+                descending: new[] { false, false, true });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderStatusHistories_Status",
+                schema: "ordering",
+                table: "OrderStatusHistories",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_GatewayTransactionId",
+                schema: "ordering",
+                table: "Payments",
+                column: "GatewayTransactionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderId",
+                schema: "ordering",
+                table: "Payments",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_PaymentDate",
+                schema: "ordering",
+                table: "Payments",
+                column: "PaymentDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_Status",
+                schema: "ordering",
+                table: "Payments",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_OrderId",
+                schema: "ordering",
+                table: "Shipments",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_OrderId_Status",
+                schema: "ordering",
+                table: "Shipments",
+                columns: new[] { "OrderId", "Status" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_ShippedDate",
+                schema: "ordering",
+                table: "Shipments",
+                column: "ShippedDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_Status",
+                schema: "ordering",
+                table: "Shipments",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_TrackingNumber",
+                schema: "ordering",
+                table: "Shipments",
+                column: "TrackingNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoices",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "OrderItems",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "OrderStatusHistories",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "Payments",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "Shipments",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "ordering");
+        }
+    }
+}

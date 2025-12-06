c3f5eaaf34d199e4f8a94cbae0c578e92a0ce220
el2toro <vasea.badragan@gmail.com>
2025-12-06 10:03:27 +0100

    Implement create order handler

--- Services/Ordering/Ordering.Application/Handlers/Commands/CreateOrderHandler.cs
+++ Services/Ordering/Ordering.Application/Handlers/Commands/CreateOrderHandler.cs
@@ -1,16 +1,49 @@
 ﻿using BuildingBlocks.CQRS;
+using Mapster;
 using MassTransit;
+using Ordering.Application.DTOs;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.Entities;
 using Ordering.Domain.Events;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Application.Handlers.Commands;
 
-public record CreateOrderCommand() : ICommand<CreateOrderResult>;
-public record CreateOrderResult();
-public class CreateOrderHandler(IPublishEndpoint publishedEndpoint) : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+public record CreateOrderCommand(
+    Guid CustomerId,
+    Address ShippingAddress,
+    Address BillingAddress,
+    string OrderNumber,
+    string Currency,
+    string CustomerNotes,
+    List<OrderItemDto> OrderItems) : ICommand<CreateOrderResult>;
+public record CreateOrderResult(OrderDetailsDto Order);
+public class CreateOrderHandler(IOrderingRepository orderingRepository,
+    IPublishEndpoint publishedEndpoint)
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
     public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
     {
+
+
+        var order = Order.CreateDraft(CustomerId.Of(command.CustomerId.ToString()),
+            OrderNumber.Of(command.OrderNumber),
+            command.ShippingAddress,
+            command.BillingAddress,
+            Currency.EUR,
+            command.CustomerNotes);
+
+        foreach (var item in command.OrderItems)
+        {
+            //order.AddOrderItem(item);
+        }
+
+        var createdOrder = await orderingRepository.CreateOrderAsync(order, cancellationToken);
+
         await publishedEndpoint.Publish<OrderCreatedEvent>("result", cancellationToken);
-        return new CreateOrderResult();
+
+        var result = createdOrder.Adapt<OrderDetailsDto>();
+
+        return new CreateOrderResult(result);
     }
 }

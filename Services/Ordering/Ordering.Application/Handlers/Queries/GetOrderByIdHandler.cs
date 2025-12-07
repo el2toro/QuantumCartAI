2e9bf809e64a8c26c5816e80b538cfc722e15dea
el2toro <vasea.badragan@gmail.com>
2025-12-07 17:16:16 +0100

    Implement GetOderById handler

---
+++ Services/Ordering/Ordering.Application/Handlers/Queries/GetOrderByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.CQRS;
+using Mapster;
+using Ordering.Application.DTOs;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Handlers.Queries;
+
+public record GetOrderByIdQuery(Guid OrderId) : IQuery<GetOrderByIdResult>;
+public record GetOrderByIdResult(OrderDetailsDto Order);
+
+public class GetOrderByIdHandler(IOrderingRepository orderingRepository)
+    : IQueryHandler<GetOrderByIdQuery, GetOrderByIdResult>
+{
+    public async Task<GetOrderByIdResult> Handle(GetOrderByIdQuery query, CancellationToken cancellationToken)
+    {
+        var order = await orderingRepository.GetOrderByIdAsync(OrderId.Of(query.OrderId.ToString()), cancellationToken);
+        var result = order.Adapt<OrderDetailsDto>();
+
+        return new GetOrderByIdResult(result);
+    }
+}

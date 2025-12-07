8fc627ff8b8695943360a7ba11d93dc089024709
el2toro <vasea.badragan@gmail.com>
2025-12-07 17:18:45 +0100

    Update contract

--- Services/Ordering/Ordering.Application/Interfaces/IOrderingRepository.cs
+++ Services/Ordering/Ordering.Application/Interfaces/IOrderingRepository.cs
@@ -6,6 +6,6 @@ namespace Ordering.Application.Interfaces;
 public interface IOrderingRepository
 {
     Task<IEnumerable<Order>> GetOrdersAsync(CustomerId customerId, CancellationToken cancellationToken);
-    Task<Order> GetOrderByIdAsync(CustomerId customerId, OrderId orderId, CancellationToken cancellationToken);
+    Task<Order> GetOrderByIdAsync(OrderId orderId, CancellationToken cancellationToken);
     Task<Order> CreateOrderAsync(Order order, CancellationToken cancellationToken);
 }

3bb4b3fe5177637b2b7d1478bbed0d281ee7db6f
el2toro <vasea.badragan@gmail.com>
2025-12-08 22:26:55 +0100

    Implement PaymentSucceededEventHandler

---
+++ Services/Ordering/Ordering.Application/Handlers/Events/PaymentSucceededEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Handlers.Events;
+
+public class PaymentSucceededEventHandler(IOrderingRepository orderingRepository)
+    : IConsumer<PaymentSucceededEvent>
+{
+    public async Task Consume(ConsumeContext<PaymentSucceededEvent> context)
+    {
+        var paymentSucceededEvent = context.Message;
+        var order = await orderingRepository
+            .GetOrderByIdAsync(OrderId.Of(paymentSucceededEvent.OrderId.ToString()), CancellationToken.None);
+
+        order.AddPayment(paymentSucceededEvent.Amount, paymentSucceededEvent.PaymentMethod);
+        order.Confirm();
+    }
+}

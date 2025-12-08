c3a43803caa2b62668dd845ced4f77da27d85a18
el2toro <vasea.badragan@gmail.com>
2025-12-08 22:25:42 +0100

    Configure cors

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,7 @@ using BuildingBlocks.Messaging.MassTransit;
 using Carter;
 using Microsoft.EntityFrameworkCore;
 using Ordering.Application.Handlers.Commands;
+using Ordering.Application.Handlers.Events;
 using Ordering.Application.Interfaces;
 using Ordering.Application.Mappings;
 using Ordering.Infrastructure.Data;
@@ -17,6 +18,7 @@ builder.Services.AddMediatR(config =>
     //config.RegisterServicesFromAssembly(assembly!);
     config.RegisterServicesFromAssembly(typeof(CreateOrderHandler).Assembly);
     config.RegisterServicesFromAssembly(typeof(CancelOrderHandler).Assembly);
+    config.RegisterServicesFromAssembly(typeof(PaymentSucceededEventHandler).Assembly);
 });
 
 builder.Services.AddDbContext<OrderingDbContext>(options =>
@@ -24,13 +26,29 @@ builder.Services.AddDbContext<OrderingDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
 });
 
+const string corsPolicy = "luxe-ecommerce-policy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy, policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()     // needed if sending cookies or Authorization headers
+            .SetIsOriginAllowed(origin => true); // allow all origins (unsafe for prod, adjust below)
+    });
+});
+
 builder.Services.AddMessageBroker(builder.Configuration, [typeof(CreateOrderHandler).Assembly]);
 
 MapsterConfig.RegisterMappings();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(corsPolicy);
+
 app.MapCarter();
 
 app.Run();

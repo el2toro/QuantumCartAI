58cddfebaa738e6c8f8db12fc4cf9a22066c27e6
el2toro <vasea.badragan@gmail.com>
2025-12-10 20:40:49 +0100

    Implement chat endpoint and handler

---
+++ Services/Chat/Chat.API/Messages/SendMessage/SendMessageEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Carter;
+using MediatR;
+
+namespace Chat.API.Messages.SendMessage;
+
+public class SendMessageEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("messages", async (string message, ISender sender) =>
+        {
+            await sender.Send(new SendMessageCommand(message));
+            return Results.Ok();
+        });
+    }
+}
---
+++ Services/Chat/Chat.API/Messages/SendMessage/SendMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+using Chat.API.Repositories;
+
+namespace Chat.API.Messages.SendMessage;
+
+public record SendMessageCommand(string Message) : ICommand<SendMessageResult>;
+public record SendMessageResult();
+
+public class SendMessageHandler(IMessageRepository messageRepository)
+    : ICommandHandler<SendMessageCommand, SendMessageResult>
+{
+    public async Task<SendMessageResult> Handle(SendMessageCommand command, CancellationToken cancellationToken)
+    {
+        await messageRepository.SendMessage(command.Message, cancellationToken);
+        return new SendMessageResult();
+    }
+}

8d65cc9311fc4c596c26efaa7ec069692f87dcb3
el2toro <vasea.badragan@gmail.com>
2025-12-10 20:43:55 +0100

    Register services and configure mediatr

--- Services/Chat/Chat.API/Program.cs
+++ Services/Chat/Chat.API/Program.cs
@@ -1,10 +1,22 @@
+using Carter;
+using Chat.API.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
 

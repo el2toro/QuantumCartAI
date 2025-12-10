f267e13f012a5a9ff88658bf4d8a3d1a7de41d0b
el2toro <vasea.badragan@gmail.com>
2025-12-10 20:41:09 +0100

    Define message repository

---
+++ Services/Chat/Chat.API/Repositories/MessageRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Chat.API.Repositories;
+
+public interface IMessageRepository
+{
+    Task SendMessage(string message, CancellationToken cancellationToken);
+}
+
+public class MessageRepository : IMessageRepository
+{
+    public Task SendMessage(string message, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}

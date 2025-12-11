2d5a4a7d2b3917877eaa1461a7286cfaad9403ab
el2toro <vasea.badragan@gmail.com>
2025-12-11 13:51:45 +0100

    Add discount db context

---
+++ Services/Discount/Discount.gRPC/Data/DiscountDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+public class DiscountDbContext : DbContext
+{
+    public DiscountDbContext(DbContextOptions<DiscountDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Models.Discount> Discounts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Models.Discount>(entity =>
+        {
+            entity.HasKey(d => d.Id);
+            entity.HasIndex(d => d.ProductId);
+            entity.HasIndex(d => d.CouponCode);
+            entity.HasIndex(d => new { d.IsActive, d.StartDate, d.EndDate });
+
+            entity.Property(d => d.Amount)
+                .HasPrecision(18, 2);
+
+            entity.Property(d => d.MinPurchaseAmount)
+                .HasPrecision(18, 2);
+        });
+    }
+}

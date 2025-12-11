d9b5053c80d8f52562ac3da55efdfac981f364fe
el2toro <vasea.badragan@gmail.com>
2025-12-11 20:02:52 +0100

    Implement cart repository

---
+++ Services/Cart/Cart.Infrastructure/Repositories/CartRepository.cs
@@ -0,0 +1,26 @@
+﻿using Cart.Domain.Interfaces;
+using Cart.Domain.ValueObjects;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Cart.Infrastructure.Repositories;
+
+public class CartRepository(IDistributedCache distributedCache) : ICartRepository
+{
+    public async Task<Domain.Entities.Cart?> GetByIdAsync(Guid cartId)
+    {
+        var cartString = await distributedCache.GetStringAsync(cartId.ToString());
+
+        return string.IsNullOrWhiteSpace(cartString)
+             ? JsonSerializer.Deserialize<Domain.Entities.Cart>(cartString!)
+             : new Domain.Entities.Cart(CartId.From(Guid.Empty.ToString()), Currency.None);
+    }
+
+    public async Task SaveAsync(Domain.Entities.Cart cart)
+    {
+        if (cart is not null)
+        {
+            await distributedCache.SetStringAsync(cart.CustomerId.ToString(), JsonSerializer.Serialize(cart));
+        }
+    }
+}

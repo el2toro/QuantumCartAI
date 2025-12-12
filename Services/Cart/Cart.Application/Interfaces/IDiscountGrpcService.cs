9b130bec036cb62d213d298466ecb2fea6b9ff9b
el2toro <vasea.badragan@gmail.com>
2025-12-12 13:28:58 +0100

    Define contract

---
+++ Services/Cart/Cart.Application/Interfaces/IDiscountGrpcService.cs
@@ -0,0 +1,10 @@
+﻿using Cart.Application.Dtos;
+
+namespace Cart.Application.Interfaces;
+
+public interface IDiscountGrpcService
+{
+    Task GetDiscountById(Guid discountId);
+    Task<IEnumerable<AppliedDiscountDto>> GetProductDiscounts(Guid productId);
+    Task<AppliedDiscountDto> GetDiscountByCouponCode(string couponCode);
+}

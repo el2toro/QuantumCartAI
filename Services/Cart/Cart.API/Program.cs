1455216bcecd1fd48dbcf9ff19dc0dc31e8494cd
el2toro <vasea.badragan@gmail.com>
2025-12-12 13:26:09 +0100

    Register services

--- Services/Cart/Cart.API/Program.cs
+++ Services/Cart/Cart.API/Program.cs
@@ -1,20 +1,36 @@
 using BuildingBlocks.Messaging.MassTransit;
+using Cart.API.Middleware;
 using Cart.API.Services;
+using Cart.API.Session;
 using Cart.Application.Handlers.Commands;
 using Cart.Application.Handlers.Events;
+using Cart.Application.Handlers.Queries;
 using Cart.Application.Interfaces;
+using Cart.Domain.Interfaces;
+using Cart.Infrastructure.Repositories;
 using Catalog.gRPC;
 using System.Reflection;
+using static DiscountService.gRPC.DiscountService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<CurrentSession>();
+
 builder.Services.AddScoped<ICatalogGrpcService, CatalogGrpcServiceClient>();
+builder.Services.AddScoped<IDiscountGrpcService, DiscountGrpcServiceClient>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(cfg =>
 {
-    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
     cfg.RegisterServicesFromAssembly(typeof(AddItemHandler).Assembly);
+    cfg.RegisterServicesFromAssembly(typeof(CartCheckoutHandler).Assembly);
+    cfg.RegisterServicesFromAssembly(typeof(GetCartQueryHandler).Assembly);
+    cfg.RegisterServicesFromAssembly(typeof(CartItemAddedEventHandler).Assembly);
 });
 
 builder.Services.AddStackExchangeRedisCache(options =>
@@ -48,10 +64,40 @@ builder.Services.AddGrpcClient<CatalogQueryService.CatalogQueryServiceClient>(op
     return handler;
 });
 
+builder.Services.AddGrpcClient<DiscountServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});
+
+const string corsPolicy = "luxe-eccomerce-policy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy, policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()     // needed if sending cookies or Authorization headers
+            .SetIsOriginAllowed(origin => true); // allow all origins (unsafe for prod, adjust below)
+    });
+});
+
 var app = builder.Build();
 
+app.UseAnonymousSession();
+app.UseCors(corsPolicy);
 // Configure the HTTP request pipeline.
 app.MapCarter();
-app.MapGet("grpc", () => "OK");
+
 app.Run();
 

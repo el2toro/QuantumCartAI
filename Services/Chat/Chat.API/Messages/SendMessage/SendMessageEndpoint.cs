ce6c5f2524fa7737d8a915e2438ac212e44c2694
el2toro <vasea.badragan@gmail.com>
2026-02-12 17:32:00 +0100

    Return proper result on sendMessage

--- Services/Chat/Chat.API/Messages/SendMessage/SendMessageEndpoint.cs
+++ Services/Chat/Chat.API/Messages/SendMessage/SendMessageEndpoint.cs
@@ -3,14 +3,15 @@ using MediatR;
 
 namespace Chat.API.Messages.SendMessage;
 
+public record SendMessageRequest(string Message);
 public class SendMessageEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("messages", async (string message, ISender sender) =>
+        app.MapPost("messages", async (SendMessageRequest request, ISender sender) =>
         {
-            await sender.Send(new SendMessageCommand(message));
-            return Results.Ok();
+            var result = await sender.Send(new SendMessageCommand(request.Message));
+            return Results.Ok(result.Message);
         });
     }
 }
--- Services/Chat/Chat.API/Messages/SendMessage/SendMessageHandler.cs
+++ Services/Chat/Chat.API/Messages/SendMessage/SendMessageHandler.cs
@@ -4,14 +4,14 @@ using Chat.API.Repositories;
 namespace Chat.API.Messages.SendMessage;
 
 public record SendMessageCommand(string Message) : ICommand<SendMessageResult>;
-public record SendMessageResult();
+public record SendMessageResult(string Message);
 
 public class SendMessageHandler(IMessageRepository messageRepository)
     : ICommandHandler<SendMessageCommand, SendMessageResult>
 {
     public async Task<SendMessageResult> Handle(SendMessageCommand command, CancellationToken cancellationToken)
     {
-        await messageRepository.SendMessage(command.Message, cancellationToken);
-        return new SendMessageResult();
+        var message = await messageRepository.SendMessage(command.Message, cancellationToken);
+        return new SendMessageResult(message);
     }
 }

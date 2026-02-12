6acf572780af588f6d76dba0919fc9932c4cd477
el2toro <vasea.badragan@gmail.com>
2026-02-12 17:23:58 +0100

    Add and configure yarp gateway

---
+++ ApiGateway/YarpApiGateway/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        //In an interval of 10 second can be sended 5 requasts as limit
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGet("/", () => "Yarp Gateway is Healthy!");
+
+app.UseRateLimiter();
+app.MapReverseProxy();
+
+app.Run();
+

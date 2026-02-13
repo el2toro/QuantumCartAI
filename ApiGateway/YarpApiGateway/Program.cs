b0fc5f855a909731526a82c3c388112dc0815720
el2toro <vasea.badragan@gmail.com>
2026-02-13 16:20:57 +0100

    Configure cors and add auth routes in yarp gateway

--- ApiGateway/YarpApiGateway/Program.cs
+++ ApiGateway/YarpApiGateway/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.RateLimiting;
+using QuantumCartAI.Shared.Infrastructure.AspNetCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddECommerceAspNetInfrastructure();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
@@ -16,13 +18,48 @@ builder.Services.AddRateLimiter(rateLimiterOptions =>
     });
 });
 
+const string corsPolicy = "luxe-ecommerce-policy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy, policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.Use(async (context, next) =>
+{
+    if (!context.Request.Cookies.TryGetValue("anonymous-id", out var anonymousId))
+    {
+        anonymousId = Guid.NewGuid().ToString();
+        context.Response.Cookies.Append("anonymous-id", anonymousId,
+            new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = context.Request.IsHttps,
+                SameSite = SameSiteMode.None,
+                Expires = DateTimeOffset.UtcNow.AddDays(90)
+            });
+    }
+
+    context.Request.Headers["X-Anonymous-Id"] = anonymousId;
+
+    await next();
+});
+
+
 app.MapGet("/", () => "Yarp Gateway is Healthy!");
 
 app.UseRateLimiter();
 app.MapReverseProxy();
+app.UseCors(corsPolicy);
 
 app.Run();
 

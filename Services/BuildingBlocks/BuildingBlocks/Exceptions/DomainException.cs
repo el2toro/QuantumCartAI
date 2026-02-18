8f368a0aacf86eb62cef1636659e13cb5d62c3b9
el2toro <vasea.badragan@gmail.com>
2026-02-18 19:27:07 +0100

    Add global exception handler

---
+++ Services/BuildingBlocks/BuildingBlocks/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Exceptions;
+
+public class DomainException : Exception
+{
+    public virtual int StatusCode => StatusCodes.Status409Conflict;
+    public virtual string Title => "Business Rule Violation";
+
+    protected DomainException(string message) : base(message) { }
+}

261ee055eaa0d7f9dd59153a4b1951983c17b895
el2toro <vasea.badragan@gmail.com>
2026-02-18 19:34:16 +0100

    Implement GetProductsById endpoint and handler

---
+++ Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductsById;
+
+public class GetProductsByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("catalog/products", async (Guid[] productIds, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsByIdQuery(productIds));
+
+            return Results.Ok(result.Products);
+        })
+       .WithDisplayName("GetProductsById")
+       .WithDescription("GetProductsById")
+       .Produces(StatusCodes.Status200OK)
+       .WithSummary("Get Products By Id");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog.API.Products.GetProductsById;
+
+public record GetProductsByIdQuery(Guid[] ProductIds) : IQuery<GetProductsByIdResult>;
+public record GetProductsByIdResult(IEnumerable<ProductDto> Products);
+public class GetProductsByIdHandler(IProductRepository productRepository)
+    : IQueryHandler<GetProductsByIdQuery, GetProductsByIdResult>
+{
+    public async Task<GetProductsByIdResult> Handle(GetProductsByIdQuery query, CancellationToken cancellationToken)
+    {
+        var products = await productRepository.GetProductsByIdAsync(query.ProductIds, cancellationToken);
+        var result = products.Adapt<IEnumerable<ProductDto>>();
+
+        return new GetProductsByIdResult(result);
+    }
+}

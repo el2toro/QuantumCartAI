e3f77fdb717d5ca8aa9e52115e9ce37a04ea1c69
el2toro <vasea.badragan@gmail.com>
2026-02-18 19:33:51 +0100

    Use proper CQRS implementation

--- Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,9 +1,9 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : ICommand<GetProductsResult>;
+public record GetProductsQuery() : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 public class GetProductsHandler(IProductRepository productRepository)
-    : ICommandHandler<GetProductsQuery, GetProductsResult>
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {

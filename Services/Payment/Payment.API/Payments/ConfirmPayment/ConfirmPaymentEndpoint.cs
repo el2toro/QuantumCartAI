d0124b4f0d77ae668fd764299d25c8fd6319d36e
el2toro <vasea.badragan@gmail.com>
2026-02-19 19:23:53 +0100

    Implement confirm payment endpoint and handler

---
+++ Services/Payment/Payment.API/Payments/ConfirmPayment/ConfirmPaymentEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Payment.API.Payments.ConfirmPayment;
+
+public class ConfirmPaymentEndpoint : ICarterModule
+{
+    public record ConfirmPaymentRequest(Guid OrderId, Guid CustomerId, decimal Amount, string PaymentMethod);
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/payment/confirm", async (ConfirmPaymentRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<ConfirmPaymentCommand>();
+            await sender.Send(command);
+
+            return Results.Ok();
+        });
+    }
+}
---
+++ Services/Payment/Payment.API/Payments/ConfirmPayment/ConfirmPaymentHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.CQRS;
+using BuildingBlocks.Messaging.Events;
+using Mapster;
+using MassTransit;
+
+namespace Payment.API.Payments.ConfirmPayment;
+
+public record ConfirmPaymentCommand(Guid OrderId, Guid CustomerId, decimal Amount, string PaymentMethod)
+    : ICommand<ConfirmPaymentResult>;
+public record ConfirmPaymentResult();
+
+public class ConfirmPaymentHandler(IPublishEndpoint publishEndpoint)
+    : ICommandHandler<ConfirmPaymentCommand, ConfirmPaymentResult>
+{
+
+    public async Task<ConfirmPaymentResult> Handle(ConfirmPaymentCommand command, CancellationToken cancellationToken)
+    {
+        var paymentSucceededEvent = command.Adapt<PaymentSucceededEvent>();
+        await publishEndpoint.Publish(paymentSucceededEvent);
+
+        return new ConfirmPaymentResult();
+    }
+}

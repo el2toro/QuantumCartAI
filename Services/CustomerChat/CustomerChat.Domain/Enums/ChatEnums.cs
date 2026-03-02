06993439ea9faf6d1e3f7ff854bd2dd8f289a9b8
el2toro <vasea.badragan@gmail.com>
2026-03-02 19:05:06 +0100

    Add domain enums

---
+++ Services/CustomerChat/CustomerChat.Domain/Enums/ChatEnums.cs
@@ -0,0 +1,34 @@
+﻿namespace CustomerChat.Domain.Enums;
+
+public enum ConversationStatus
+{
+    Open = 1,
+    PendingAgent = 2,
+    AssignedToAgent = 3,
+    Resolved = 4,
+    Closed = 5
+}
+
+public enum MessageType
+{
+    Text = 1,
+    Image = 2,
+    File = 3,
+    SystemNotification = 4
+}
+
+public enum SenderType
+{
+    Customer = 1,
+    Agent = 2,
+    Bot = 3,
+    System = 4
+}
+
+public enum AgentStatus
+{
+    Online = 1,
+    Busy = 2,
+    Away = 3,
+    Offline = 4
+}
